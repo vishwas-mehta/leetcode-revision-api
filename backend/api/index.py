b9cee170a6b5372c427df119b70cdc3a1cf9c169
vishwas-mehta <vishwas.mehta.work@gmail.com>
2026-01-06 23:09:15 +0530

    Added cors and created the frontend to make the app run simultaniuosly on frontend and backend

--- backend/api/index.py
+++ backend/api/index.py
@@ -2,8 +2,17 @@ from fastapi import FastAPI, HTTPException
 from fastapi.responses import HTMLResponse
 import requests
 import random
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get('/', response_class=HTMLResponse)
 def root():
